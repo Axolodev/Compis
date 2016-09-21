ac80f6cf46b8df3534c7101c8ce773addd8a5d67
Roberto Ruiz <rroberto.rruiz@gmail.com>
2016-09-21 17:15:08 -0500

    Push bien rápido

--- scanner.py
+++ scanner.py
@@ -5,103 +5,124 @@
 # -----------------------------------------------------------------------------
 
 tokens = (
-    'INICIO', 'FUNCION', 'NORTE', 'SUR', 'ESTE', 'OESTE',
-    'MIENTRAS', 'SI', 'SI_NO', 'FLOTANTE',
-    'STRING', 'VERDADERO', 'CAMINA', 'GIRA',
-    'MIRA', 'A_STRING', 'REINICIAR', 'INPUT',
-    'OUTPUT', 'A_ENTERO', 'ANCHO', 'ALTO',
-    'A_FLOTANTE', 'SALTA_A', 'FALSO', 'ENTERO', 'MAS', 'MENOS', 'POR',
-    'ENTRE', 'RESIDUO', 'MENOR_QUE', 'MAYOR_QUE',
-    'MENOR_IGUAL', 'MAYOR_IGUAL', 'DIFERENTE_DE', 'IGUAL_A',
-    'AND', 'OR', 'IGUAL', 'PARENTESIS_IZQ',
-    'PARENTESIS_DER', 'LLAVE_IZQ', 'LLAVE_DER', 'CORCHETE_IZQ',
-    'CORCHETE_DER','PUNTO_COMA', 'COMILLA', 'COMA', 'ID', 'CTE_I', 'PUNTO',
-    'CTE_S',
+    'KW_INICIO', 'KW_FUNCION', 'KW_NORTE', 'KW_SUR',
+    'KW_ESTE', 'KW_OESTE', 'KW_MIENTRAS', 'KW_SI',
+    'KW_SI_NO', 'KW_FLOTANTE', 'KW_STRING', 'KW_VERDADERO',
+    'KW_CAMINA', 'KW_GIRA', 'KW_MIRA', 'KW_A_STRING',
+    'KW_REINICIAR', 'KW_INPUT', 'KW_OUTPUT', 'KW_A_ENTERO',
+    'KW_ANCHO', 'KW_ALTO', 'KW_A_FLOTANTE', 'KW_SALTA_A',
+    'KW_FALSO', 'KW_ENTERO',
+
+    'OP_SUMA', 'OP_RESTA', 'OP_MULTIPLICACION', 'OP_DIVISION', 'OP_RESIDUO',
+    'OP_MENOR_QUE', 'OP_MAYOR_QUE', 'OP_MENOR_IGUAL', 'OP_MAYOR_IGUAL',
+    'OP_DIFERENTE_DE', 'OP_IGUAL_A', 'OP_AND', 'OP_OR', 'OP_ASIGNACION',
+    'OP_PARENTESIS_IZQ', 'OP_PARENTESIS_DER', 'OP_LLAVE_IZQ', 'OP_LLAVE_DER',
+    'OP_CORCHETE_IZQ', 'OP_CORCHETE_DER', 'OP_PUNTO_COMA', 'OP_COMILLA',
+    'OP_COMA', 'OP_PUNTO',
+
+    'ID', 'CTE_I', 'CTE_F', 'CTE_S',
 )
 
 reserved = {
-    'inicio' : 'INICIO',
-    'funcion' : 'FUNCION',
-    'norte' : 'NORTE',
-    'sur' : 'SUR',
-    'este' : 'ESTE',
-    'oeste' : 'OESTE',
-    'mientras' : 'MIENTRAS',
-    'si' : 'SI',
-    'si_no' : 'SI_NO',
-    'flotante' : 'FLOTANTE',
-    'string' : 'STRING',
-    'verdadero' : 'VERDADERO',
-    'falso' : 'FALSO',
-    'camina' : 'CAMINA',
-    'gira' : 'GIRA',
-    'mira' : 'MIRA',
-    'a_string' : 'A_STRING',
-    'reiniciar' : 'REINICIAR',
-    'input' : 'INPUT',
-    'output' : 'OUTPUT',
-    'a_entero' : 'A_ENTERO',
-    'ancho' : 'ANCHO',
-    'alto' : 'ALTO',
-    'a_flotante' : 'A_FLOTANTE',
-    'salta_a' : 'SALTA_A',
-    'entero' : 'ENTERO',
+    'inicio': 'KW_INICIOP_O',
+    'funcion': 'KW_FUNCION',
+    'norte': 'KW_NORTE',
+    'sur': 'KW_SUR',
+    'este': 'KW_ESTE',
+    'oeste': 'KW_OESTE',
+    'mientras': 'KW_MIENTRAS',
+    'si': 'KW_SI',
+    'si_no': 'KW_SI_NO',
+    'flotante': 'KW_FLOTANTE',
+    'string': 'KW_STRING',
+    'verdadero': 'KW_VERDADERO',
+    'falso': 'KW_FALSO',
+    'camina': 'KW_CAMINA',
+    'gira': 'KW_GIRA',
+    'mira': 'KW_MIRA',
+    'a_string': 'KW_A_STRING',
+    'reiniciar': 'KW_REINICIAR',
+    'input': 'KW_INPUT',
+    'output': 'KW_OUTPUT',
+    'a_entero': 'KW_A_ENTERO',
+    'ancho': 'KW_ANCHO',
+    'alto': 'KW_ALTO',
+    'a_flotante': 'KW_A_FLOTANTE',
+    'salta_a': 'KW_SALTA_A',
+    'entero': 'KW_ENTERO',
 }
 
 # Tokens
-t_SEMICOLON = r'\;'
-t_PERIOD = r'[\.]'
-t_COMMA = r'[\,]'
-t_COLON = r'\:'
-t_LEFT_BRACKET = r'\{'
-t_RIGHT_BRACKET = r'\}'
-t_LEFT_PARENTHESIS = r'\('
-t_RIGHT_PARENTHESIS = r'\)'
-t_EQUAL_OPERATOR  = r'\='
-t_COMPARISON_OPERATOR = r'[<][>]|[>]|[<]'
-t_EXP_OPERATOR = r'\+|\-'
-t_TERM_OPERATOR = r'\*|\/'
+t_OP_PUNTO_Y_COMA = r'\;'
+t_OP_PUNTO = r'[\.]'
+t_OP_COMA = r'[\,]'
+t_OP_LLAVE_IZQUIERDA = r'\{'
+t_OP_LLAVE_DERECHA = r'\}'
+t_OP_PARENTESIS_IZQUIERDO = r'\('
+t_OP_PARENTESIS_DERECHO = r'\)'
+t_OP_OPERADOR_ASIGNACION = r'\='
+t_OP_MENOR_QUE = r'[<]'
+t_OP_MAYOR_QUE = r'[>]'
+t_OP_MAYOR_IGUAL = r'[>][=]'
+t_OP_MENOR_IGUAL = r'[<][=]'
+t_OP_IGUAL_A = r'[=][=]'
+t_OP_DIFERENTE_A = r'![=]'
+t_OP_EXP_OPERATOR = r'\+|\-'
+t_OP_TERM_OPERATOR = r'\*|\/'
+
+
 def t_CONST_NUMBER_FLOAT(t):
     r'[0-9]+\.[0-9]+'
     t.value = float(t.value)
     return t
+
 def t_CONST_NUMBER_INT(t):
     r'\d+'
     t.value = int(t.value)
     return t
+
 def t_IDENTIFIER(t):
     r'[a-zA-Z_][a-zA-Z0-9_]*'
     if t.value in reserved:
         t.type = reserved[t.value]
     return t
+
+
 t_CONST_STRING = r'\"[A-Za-z0-9_\(\)\{\}\[\]\<\>\!]*\"'
 
 # Ignored characters
 t_ignore = " \t"
 
+
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += t.value.count("\n")
+
 
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
 
+
 # Build the lexer
 import ply.lex as lex
+
 lexer = lex.lex()
 
 start = 'program'
 
+
 def p_empty(p):
     'empty :'
     pass
+
 
 def p_program(p):
     '''
     program : KEYWORD_PROGRAM IDENTIFIER COLON program_vars block_start
     '''
     print("Program start\n\tIdentifier:" + p[2] + "\n")
+
 
 def p_program_vars(p):
     '''
@@ -110,17 +131,20 @@ def p_program_vars(p):
     '''
     pass
 
+
 def p_vars(p):
     '''
     vars : KEYWORD_VAR var_declaration_start
     '''
     print("VARIABLES:\n")
 
+
 def p_var_declaration_start(p):
     '''
     var_declaration_start : IDENTIFIER var_declaration_end
     '''
     print("\t- Identifier: " + p[1] + "\n")
+
 
 def p_var_declaration_end(p):
     '''
@@ -130,12 +154,12 @@ def p_var_declaration_end(p):
     pass
 
 
-
 def p_var_type_start(p):
     '''
     var_type_start : COLON var_type_def SEMICOLON var_type_end
     '''
     pass
+
 
 def p_var_type_end(p):
     '''
@@ -152,11 +176,13 @@ def p_var_type_def(p):
     '''
     print("type definition: " + p[1] + "\n")
 
+
 def p_block_start(p):
     '''
     block_start : LEFT_BRACKET block_end
     '''
     print("block start\n")
+
 
 def p_block_end(p):
     '''
@@ -165,11 +191,13 @@ def p_block_end(p):
     '''
     pass
 
+
 def p_block_end_output(p):
     '''
     block_end_output : empty
     '''
     print("block end\n")
+
 
 def p_statement(p):
     '''
@@ -179,17 +207,20 @@ def p_statement(p):
     '''
     pass
 
+
 def p_assign(p):
     '''
     assign : IDENTIFIER EQUAL_OPERATOR expression SEMICOLON
     '''
     print(p[1] + " = ")
 
+
 def p_expression(p):
     '''
     expression : exp_start exp_comparison
     '''
     pass
+
 
 def p_exp_comparison(p):
     '''
@@ -198,17 +229,20 @@ def p_exp_comparison(p):
     '''
     pass
 
+
 def p_exp_comparison_output(p):
     '''
     exp_comparison_output : COMPARISON_OPERATOR
     '''
     print(" " + p[1])
 
+
 def p_exp_start(p):
     '''
     exp_start : term_start exp_end
     '''
     pass
+
 
 def p_exp_end(p):
     '''
@@ -217,20 +251,24 @@ def p_exp_end(p):
     '''
     pass
 
+
 def p_exp_end_f(p):
     '''
     exp_end_f : EXP_OPERATOR exp_start
     '''
     print(' ' + p[1] + ' ')
 
+
 def p_error(t):
     print("Syntax error at '%s'" % t.value)
+
 
 def p_term_start(p):
     '''
     term_start : factor term_end
     '''
     pass
+
 
 def p_term_end(p):
     '''
@@ -239,11 +277,13 @@ def p_term_end(p):
     '''
     pass
 
+
 def p_term_end_output(p):
     '''
     term_end_output : TERM_OPERATOR term_start
     '''
     print(p[1] + ' ')
+
 
 def p_output(p):
     '''
@@ -258,6 +298,7 @@ def p_output_output(p):
     '''
     print("print (")
 
+
 def p_write_start(p):
     '''
     write_start : write_start_output write_end
@@ -265,11 +306,13 @@ def p_write_start(p):
     '''
     pass
 
+
 def p_write_start_output(p):
     '''
     write_start_output : CONST_STRING
     '''
     print(p[1] + ' ')
+
 
 def p_write_end(p):
     '''
@@ -278,11 +321,13 @@ def p_write_end(p):
     '''
     print("\n")
 
+
 def p_condition(p):
     '''
     condition : KEYWORD_IF LEFT_PARENTHESIS expression RIGHT_PARENTHESIS block_start condition_else
     '''
     pass
+
 
 def p_condition_else(p):
     '''
@@ -290,6 +335,7 @@ def p_condition_else(p):
                     | empty
     '''
     pass
+
 
 def p_factor(p):
     '''
@@ -308,7 +354,9 @@ def p_constant_val(p):
     '''
     print(str(p[1]) + ' ')
 
+
 import ply.yacc as yacc
+
 parser = yacc.yacc()
 
 data = '''
@@ -329,6 +377,5 @@ f_one, f_two, f_three : float ; {
     two = one + three + 1.0;
 }
 '''
-
 
 parser.parse(data)
