c8e33d4073b0a0199da307a7b86d6d55859e9df6
Roberto Ruiz <rroberto.rruiz@gmail.com>
2016-10-02 18:25:54 -0500

    Tabla de variables...terminada?

--- TablaVariables.py
+++ TablaVariables.py
@@ -11,14 +11,25 @@ class TablaVariables:
             self.__scopeActual += 1
 
         def nuevaVariable(self, tipo, nombre):
-            if self.__listaVariables.has_key(str(self.__scopeActual) + "_" + str(nombre)):
-                # Tirar error, nombre de variable repetido
-                pass
+            if (str(self.__scopeActual) + "_" + str(nombre)) in self.__listaVariables:
+                raise ValueError("Ya existe una variable con este nombre")
             else:
-                var = Variable.Variable(tipo, nombre)
-                self.__listaVariables[str(self.__scopeActual) + "_" + str(nombre)] = var
+                var = Variable.Variable(tipo, nombre, self.__scopeActual)
+                self.__listaVariables.update({str(self.__scopeActual) + "_" + str(nombre): var})
 
+        def getVariable(self, nombre, scope):
+            if not (str(scope) + "_" + str(nombre)) in self.__listaVariables:
+                raise ValueError("La variable no existe")
+            else:
+                return self.__listaVariables[str(scope) + "_" + str(nombre)]
 
+        def setValorAVariable(self, nombre, scope, valor):
+            if not (str(scope) + "_" + str(nombre)) in self.__listaVariables:
+                raise ValueError("La variable no existe")
+            else:
+                var = self.__listaVariables.get(str(self.__scopeActual) + "_" + str(nombre))
+                var.setValor(valor)
+                self.__listaVariables.update({str(self.__scopeActual) + "_" + str(nombre): var})
 
     instancia = None
 
@@ -31,6 +42,14 @@ class TablaVariables:
         self.instancia.nuevoScope()
 
     def creaVar(self, tipo, nombre):
+        self.instancia.nuevaVariable(tipo, nombre)
+
+    def getVariable(self, nombre, scope):
+        return self.instancia.getVariable(nombre, scope)
+
+    def setValorAVariable(self, nombre, scope, valor):
+        self.instancia.setValorAVariable(nombre, scope, valor)
+
 
 
 
